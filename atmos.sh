b7f8760130820effa1e62317b3d5720d96168e9c
FLOW4215 <qingxin.feng@hotmail.com>
2023-03-02 13:53:22 +0800

    Update atmos.sh

--- atmos.sh
+++ atmos.sh
@@ -21,21 +21,21 @@ do
 	if [ "$test_type" = "ontesting" ]; then
 		echo “测试执行中。。。”
 	elif [ "$test_type" = "api_test" ]; then
-		nohup sh ${ATMOS_PATH}/script/api_test.sh > ${INIT_PATH}/log_${test_type} 2>&1 &
+		nohup sh ${ATMOS_PATH}/script/api_test.sh >> ${INIT_PATH}/log_${test_type} 2>&1 &
 	elif [ "$test_type" = "config_insert" ]; then
-		nohup sh ${ATMOS_PATH}/script/config_insert.sh > ${INIT_PATH}/log_${test_type} 2>&1 &
+		nohup sh ${ATMOS_PATH}/script/config_insert.sh >> ${INIT_PATH}/log_${test_type} 2>&1 &
 	elif [ "$test_type" = "routine_test" ]; then
-		nohup sh ${ATMOS_PATH}/script/routine_test.sh > ${INIT_PATH}/log_${test_type} 2>&1 &
+		nohup sh ${ATMOS_PATH}/script/routine_test.sh >> ${INIT_PATH}/log_${test_type} 2>&1 &
 	elif [ "$test_type" = "se_insert" ]; then
-		nohup sh ${ATMOS_PATH}/script/se_insert.sh > ${INIT_PATH}/log_${test_type} 2>&1 &
+		nohup sh ${ATMOS_PATH}/script/se_insert.sh >> ${INIT_PATH}/log_${test_type} 2>&1 &
 	elif [ "$test_type" = "se_query" ]; then
-		nohup sh ${ATMOS_PATH}/script/se_query.sh > ${INIT_PATH}/log_${test_type} 2>&1 &
+		nohup sh ${ATMOS_PATH}/script/se_query.sh >> ${INIT_PATH}/log_${test_type} 2>&1 &
 	elif [ "$test_type" = "unse_insert" ]; then
-		nohup sh ${ATMOS_PATH}/script/unse_insert.sh > ${INIT_PATH}/log_${test_type} 2>&1 &
+		nohup sh ${ATMOS_PATH}/script/unse_insert.sh >> ${INIT_PATH}/log_${test_type} 2>&1 &
 	elif [ "$test_type" = "unse_query" ]; then
-		nohup sh ${ATMOS_PATH}/script/unse_query.sh > ${INIT_PATH}/log_${test_type} 2>&1 &
+		nohup sh ${ATMOS_PATH}/script/unse_query.sh >> ${INIT_PATH}/log_${test_type} 2>&1 &
 	else
-		nohup sh ${ATMOS_PATH}/script/${test_type}.sh > ${INIT_PATH}/log_${test_type} 2>&1 &
+		nohup sh ${ATMOS_PATH}/script/${test_type}.sh >> ${INIT_PATH}/log_${test_type} 2>&1 &
 	fi
 	sleep 300s
 done
