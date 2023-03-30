726cac8384fd5e2210813522fa8ba03700e99b79
FLOW4215 <qingxin.feng@hotmail.com>
2023-03-30 11:21:41 +0800

    丰富分布式测试场景

--- script/cluster_insert.sh
+++ script/cluster_insert.sh
@@ -436,14 +436,40 @@ else
 	test_date_time=`date +%Y%m%d%H%M%S`
 	###############################普通时间序列###############################
 	echo "开始测试普通时间序列顺序写入！"
-	test_operation common noOverflow 223
+	test_operation common seq_w 223
 	echo "开始测试普通时间序列乱续写入！"
-	test_operation common isOverflow 223
+	test_operation common unseq_w 223
+	echo "开始测试普通时间序列顺序写入！"
+	test_operation common seq_rw 223
+	echo "开始测试普通时间序列乱续写入！"
+	test_operation common unseq_rw 223
 	###############################对齐时间序列###############################
 	echo "开始测试对齐时间序列顺序写入！"
-	test_operation aligned noOverflow 223
+	test_operation aligned seq_w 223
 	echo "开始测试对齐时间序列乱续写入！"
-	test_operation aligned isOverflow 223
+	test_operation aligned unseq_w 223
+	echo "开始测试对齐时间序列顺序写入！"
+	test_operation aligned seq_rw 223
+	echo "开始测试对齐时间序列乱续写入！"
+	test_operation aligned unseq_rw 223
+	###############################模板时间序列###############################
+	#echo "开始测试模板时间序列顺序写入！"
+	#test_operation template seq_w 223
+	#echo "开始测试模板时间序列乱续写入！"
+	#test_operation template unseq_w 223
+	#echo "开始测试模板时间序列顺序写入！"
+	#test_operation template seq_rw 223
+	#echo "开始测试模板时间序列乱续写入！"
+	#test_operation template unseq_rw 223
+	###############################对齐模板时间序列###############################
+	#echo "开始测试对齐模板时间序列顺序写入！"
+	#test_operation tempaligned seq_w 223
+	#echo "开始测试对齐模板时间序列乱续写入！"
+	#test_operation tempaligned unseq_w 223
+	#echo "开始测试对齐模板时间序列顺序写入！"
+	#test_operation tempaligned seq_rw 223
+	#echo "开始测试对齐模板时间序列乱续写入！"
+	#test_operation tempaligned unseq_rw 223
 	###############################测试完成###############################
 	echo "本轮测试${test_date_time}已结束."
 	update_sql="update ${TASK_TABLENAME} set ${test_type} = 'done' where commit_id = '${commit_id}'"
