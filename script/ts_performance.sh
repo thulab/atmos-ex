b25f0921540c9ce0ccef651621c8c1a0287e64b5
FLOW4215 <qingxin.feng@hotmail.com>
2023-05-07 15:57:50 +0800

    增加判断集群是否启动的循环

--- script/ts_performance.sh
+++ script/ts_performance.sh
@@ -311,7 +311,17 @@ test_operation() {
 	start_iotdb
 	sleep 10	
 	####判断IoTDB是否正常启动
-	iotdb_state=$(${TEST_IOTDB_PATH}/sbin/start-cli.sh -e "show version" | grep 'Total line number = 1')
+	for (( t_wait = 0; t_wait <= 100; t_wait++ ))
+	do
+	  iotdb_state=$(${TEST_IOTDB_PATH}/sbin/start-cli.sh -e "show version" | grep 'Total line number = 1')
+	  if [ "${iotdb_state}" = "Total line number = 1" ]; then
+		break
+		
+	  else
+		sleep 30
+		continue
+	  fi
+	done
 	if [ "${iotdb_state}" = "Total line number = 1" ]; then
 		echo "IoTDB正常启动，准备开始测试"
 	else
