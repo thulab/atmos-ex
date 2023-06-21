238b05eea2ba65cbb4959951099ced80aa80e9a3
冯庆新 <qingxin.feng@hotmail.com>
2023-06-21 09:18:13 +0800

    Update cluster_insert.sh

--- script/cluster_insert.sh
+++ script/cluster_insert.sh
@@ -441,7 +441,7 @@ test_operation() {
 	#测试结果收集写入数据库
 	rm -rf ${BM_PATH}/TestResult/csvOutput/*
 	scp -r ${ACCOUNT}@${B_IP_list[1]}:${BM_PATH}/data/csvOutput/*result.csv ${BM_PATH}/TestResult/csvOutput/
-	for ((j = 1; j <= 5; j++)); do
+	for ((j = 1; j <= 3; j++)); do
 		#收集启动后基础监控数据
 		collect_monitor_data ${j}
 		csvOutputfile=${BM_PATH}/TestResult/csvOutput/*result.csv
