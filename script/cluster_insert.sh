b7d09cb449b5a1eca88332f58ce5b2068978514f
冯庆新 <qingxin.feng@hotmail.com>
2024-12-31 20:34:29 +0800

    标点错误

--- script/cluster_insert.sh
+++ script/cluster_insert.sh
@@ -48,7 +48,7 @@ echo "检查iot-benchmark版本"
 BM_REPOS_PATH=/nasdata/repository/iot-benchmark
 BM_NEW=$(cat ${BM_REPOS_PATH}/git.properties | grep git.commit.id.abbrev | awk -F= '{print $2}')
 BM_OLD=$(cat ${BM_PATH}/git.properties | grep git.commit.id.abbrev | awk -F= '{print $2}')
-if [ "${BM_OLD}" ！= "cat: git.properties: No such file or directory" ] && [ "${BM_OLD}" != "${BM_NEW}" ]; then
+if [ "${BM_OLD}" != "cat: git.properties: No such file or directory" ] && [ "${BM_OLD}" != "${BM_NEW}" ]; then
 	rm -rf ${BM_PATH}
 	cp -rf ${BM_REPOS_PATH} ${BM_PATH}
 fi
