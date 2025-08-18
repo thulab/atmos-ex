5429dd1946a16dde5bfd5e55d8cec4b357a670f9
冯庆新 <qingxin.feng@hotmail.com>
2025-08-18 14:23:25 +0800

    Update se_query.sh

--- script/se_query.sh
+++ script/se_query.sh
@@ -307,7 +307,7 @@ test_operation() {
 			####判断IoTDB是否正常启动
 			for (( t_wait = 0; t_wait <= 10; t_wait++ ))
 			do
-			  iotdb_state=$(${TEST_IOTDB_PATH}/sbin/start-cli.sh -e "show cluster" | grep 'Total line number = 2')
+			  iotdb_state=$(${TEST_IOTDB_PATH}/sbin/start-cli.sh -u root -pw root -e "show cluster" | grep 'Total line number = 2')
 			  if [ "${iotdb_state}" = "Total line number = 2" ]; then
 				break
 			  else
