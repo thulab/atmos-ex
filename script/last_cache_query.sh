adfc010198659a2a377482a97cd64ac2b87ee5fc
冯庆新 <qingxin.feng@hotmail.com>
2026-02-07 13:31:33 +0800

    Update last_cache_query.sh

--- script/last_cache_query.sh
+++ script/last_cache_query.sh
@@ -410,6 +410,11 @@ else
 	update_sql="update ${TASK_TABLENAME} set ${test_type} = 'ontesting' where commit_id = '${commit_id}'"
 	result_string=$(mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${update_sql}")
 	echo "当前版本${commit_id}未执行过测试，即将编译后启动"
+	if [ "${author}" != "Timecho" ]; then
+		TABLENAME=${TABLENAME}
+	else
+		TABLENAME=${TABLENAME_T}
+	fi
 	init_items
 	test_date_time=`date +%Y%m%d%H%M%S`
 	p_index=$(($RANDOM % ${#protocol_list[*]}))
