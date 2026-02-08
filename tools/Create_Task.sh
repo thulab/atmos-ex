907861c6abdd1cb08324870af5fcd94a77ab004f
冯庆新 <qingxin.feng@hotmail.com>
2026-02-08 08:52:51 +0800

    Update Create_Task.sh

--- tools/Create_Task.sh
+++ tools/Create_Task.sh
@@ -53,7 +53,7 @@ else
 	# 判断是否是每周六
 	if [ "$day_of_week" -eq 6 ]; then
 		echo $date "  今天是周六，准备下派长耗时任务"
-		update_sql="update ${TABLENAME} set cluster_insert = '${str_type}',cluster_insert_2 = '${str_type}',restart_db = '${str_type}',count_ts = '${str_type}' where commit_id = '${commit_id_new}'"
+		update_sql="update ${TABLENAME} set weeklytest_insert = '${str_type}',weeklytest_query = '${str_type}',config_insert = '${str_type}',cluster_insert = '${str_type}',cluster_insert_2 = '${str_type}',restart_db = '${str_type}',count_ts = '${str_type}' where commit_id = '${commit_id_new}'"
 		result_string=$(mysql -h${MYSQLHOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${update_sql}")
 	fi
 fi
