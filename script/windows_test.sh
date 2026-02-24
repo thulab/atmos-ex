a1e0ddd1748238a17ad1dedfc0cccc62a48996d9
冯庆新 <qingxin.feng@hotmail.com>
2026-02-24 15:28:44 +0800

    Update windows_test.sh

--- script/windows_test.sh
+++ script/windows_test.sh
@@ -118,7 +118,7 @@ modify_iotdb_config() { # iotdb调整内存，关闭合并
 	
 	echo "cn_internal_address=${IoTDB_IP}" >> ${TEST_IOTDB_PATH}/conf/iotdb-system.properties
 	echo "cn_seed_config_node=${IoTDB_IP}:10710" >> ${TEST_IOTDB_PATH}/conf/iotdb-system.properties
-	echo "dn_rpc_address.*$/dn_rpc_address=${IoTDB_IP}" >> ${TEST_IOTDB_PATH}/conf/iotdb-system.properties
+	echo "dn_rpc_address=${IoTDB_IP}" >> ${TEST_IOTDB_PATH}/conf/iotdb-system.properties
 	echo "dn_internal_address=${IoTDB_IP}" >> ${TEST_IOTDB_PATH}/conf/iotdb-system.properties
 	echo "dn_seed_config_node=${IoTDB_IP}:10710" >> ${TEST_IOTDB_PATH}/conf/iotdb-system.properties
 }
