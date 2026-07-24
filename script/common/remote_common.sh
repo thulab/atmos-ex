020c7d44d6bb420ea387b137018e51227d89287b
冯庆新 <qingxin.feng@hotmail.com>
2026-07-24 18:46:08 +0800

    Fix recursive SCP to Windows hosts

--- script/common/remote_common.sh
+++ script/common/remote_common.sh
@@ -227,19 +227,17 @@ remote_windows_reset_dir() {
 }
 
 # 功能：在远程主机上执行受控的部署或检查操作
-remote_windows_copy_contents() (
+remote_windows_copy_contents() {
     local source_dir="$1"
     local host="$2"
     local destination_dir="$3"
-    local -a source_entries=()
 
     [ -d "${source_dir}" ] || die "missing local directory: ${source_dir}"
-    shopt -s dotglob nullglob
-    source_entries=("${source_dir}"/*)
-    [ "${#source_entries[@]}" -gt 0 ] || die "local directory is empty: ${source_dir}"
-    scp -r -- "${source_entries[@]}" "$(remote_target "${host}"):${destination_dir}" ||
+    [ -n "$(find "${source_dir}" -mindepth 1 -maxdepth 1 -print -quit)" ] ||
+        die "local directory is empty: ${source_dir}"
+    scp -T -r -- "${source_dir}/." "$(remote_target "${host}"):${destination_dir}" ||
         die "failed to copy directory contents to ${host}:${destination_dir}"
-)
+}
 
 # 功能：在远程主机上执行受控的部署或检查操作
 remote_windows_run_task() {
